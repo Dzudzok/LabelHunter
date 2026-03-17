9a8d527a6fc440fbee2c3f0d8a2ce9adf47a3f08
Dzudzok <mateuszdurczok01@gmail.com>
2026-03-17 08:37:18 +0100

    55

--- lp-sync/config.js
+++ lp-sync/config.js
@@ -27,6 +27,6 @@ const SMTP_CONFIG = {
 const APP_URL = 'https://labelhunter-client.onrender.com';
 
 // Set to true to disable sending emails
-const DISABLE_EMAIL = false;
+const DISABLE_EMAIL = true;
 
 module.exports = { MSSQL_CONFIG, SUPABASE_URL, SUPABASE_SERVICE_KEY, SMTP_CONFIG, APP_URL, DISABLE_EMAIL };
